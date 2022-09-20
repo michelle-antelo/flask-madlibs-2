e77596c559e37040dfc7a565b55f696e4363267f
Michelle Antelo <michelle@Michelles-MacBook-Pro.local>
2022-09-20 17:53:31 -0400

    finalized

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+from stories import story
+
+app = Flask(__name__)
+
+@app.route("/")
+def generate_input():
+    prompts = story.prompts
+    return render_template("form.html", prompts=prompts)
+
+@app.route("/story")
+def final_story():
+    text = story.generate(request.args)
+    return render_template("story.html", text=text)
